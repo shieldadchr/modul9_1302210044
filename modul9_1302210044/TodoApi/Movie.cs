7529a50e767a32f9236a07b232bd7d0381099a46
shieldadchr <127294977+shieldadchr@users.noreply.github.com>
2023-04-26 17:02:35 +0700

    Add files via upload
    menambahkan API movies

---
+++ modul9_1302210044/TodoApi/Movie.cs
@@ -0,0 +1,25 @@
+﻿namespace modul9_1302210044
+{
+    public class Movie
+    {
+        public String Title { get; set; }
+        public String Director { get; set; }
+        public String Stars1 { get; set; }
+        public String Stars2 { get; set; }
+        public String Stars3 { get; set; }
+        public String Stars4 { get; set; }
+
+        public String Description { get; set; }
+
+        public Movie(String title, String director, String stars1, String stars2, String stars3, String stars4, String description)
+        {
+            this.Title = title;
+            this.Director = director;
+            this.Stars1 = stars1;
+            this.Stars2 = stars2;
+            this.Stars3 = stars3;
+            this.Stars4 = stars4;
+            this.Description = description;
+        }
+    }
+}
